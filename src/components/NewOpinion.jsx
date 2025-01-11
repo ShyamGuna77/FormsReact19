ae4b530da8780a92ac611140ea71e704a655b6be
Shyam <gshyamprasad2001@gmail.com>
2025-01-11 18:24:18 +0530

    finished

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,35 +1,48 @@
-import { useActionState } from "react";
+import { useActionState, use } from "react";
+
+import { OpinionsContext } from "../store/opinions-context";
 
 export function NewOpinion() {
+  const { addOpinion } = use(OpinionsContext);
 
-  function handleSignup(prevState,formData) {
-        const title = formData.get('title');
-        const body = formData.get('body');
-        const userName = formData.get('userName');
+  async function shareOpinionAction(prevState, formData) {
+    const title = formData.get("title");
+    const body = formData.get("body");
+    const userName = formData.get("userName");
 
-        let errors =  [];
-        if(title.trim().length<3){
-          errors.push('Please enter a Valid Title')
-        }
+    let errors = [];
 
-        if(body.trim().length<10 && body.trim().length>300){
-          errors.push("Opinion must be between 10 and 300 characters")
-        }
+    if (title.trim().length < 5) {
+      errors.push("Title must be at least five characters long.");
+    }
 
-        if(!userName.trim()){
-          errors.push('Provide an UserName');
-        }
-        if(errors.length>0){
-          return {errors,enteredValues: {
-           title,body,userName
-          }}
-        }
+    if (body.trim().length < 10 || body.trim().length > 300) {
+      errors.push("Opinion must be between 10 and 300 characters long.");
+    }
 
-        return {errors:null}
+    if (!userName.trim()) {
+      errors.push("Please provide your name.");
+    }
 
+    if (errors.length > 0) {
+      return {
+        errors,
+        enteredValues: {
+          title,
+          body,
+          userName,
+        },
+      };
+    }
+
+    await addOpinion({ title, body, userName });
+    return { errors: null };
   }
 
-  const [formState ,formAction] = useActionState(handleSignup,{errors:null})
+  const [formState, formAction] = useActionState(shareOpinionAction, {
+    errors: null,
+  });
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
@@ -60,10 +73,11 @@ export function NewOpinion() {
           <textarea
             id="body"
             name="body"
-            defaultValue={formState.enteredValues?.body}
             rows={5}
+            defaultValue={formState.enteredValues?.body}
           ></textarea>
         </p>
+
         {formState.errors && (
           <ul className="errors">
             {formState.errors.map((error) => (
@@ -71,6 +85,7 @@ export function NewOpinion() {
             ))}
           </ul>
         )}
+
         <p className="actions">
           <button type="submit">Submit</button>
         </p>
